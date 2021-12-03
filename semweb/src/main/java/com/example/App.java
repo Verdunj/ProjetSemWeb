df69b51d86bc1bec195faf8c610a83f3f129104d
VerdunJ <verdun.joris@gmail.com>
2021-12-03 15:01:21 +0100

    pom.xml

--- semweb/src/main/java/com/example/App.java
+++ semweb/src/main/java/com/example/App.java
@@ -1,5 +1,13 @@
 package com.example;
 
+
+
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.vocabulary.VCARD;
+
 /**
  * Hello world!
  */
