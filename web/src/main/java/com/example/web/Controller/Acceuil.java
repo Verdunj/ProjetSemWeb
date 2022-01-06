dd653c4b3fe196281267265e8367d8efd8f3360c
VerdunJ <verdun.joris@gmail.com>
2022-01-06 21:38:10 +0100

    récup timestamp et temp

--- web/src/main/java/com/example/web/Controller/Acceuil.java
+++ web/src/main/java/com/example/web/Controller/Acceuil.java
@@ -37,19 +37,35 @@ public class Acceuil {
     @RequestMapping(value = "/temp")
     public String temp(Model m) {
         ConnexionFuseki conn = new ConnexionFuseki();
-        String query = "PREFIX time: <http://www.w3.org/2006/time#> PREFIX time: <http://www.w3.org/2006/time#> PREFIX ex:   <http://example/> SELECT * WHERE{?h ex:at \"Saint-Etienne\". ?h ex:hasTemp ?t.}";
+        String query = "PREFIX time: <http://www.w3.org/2006/time#> PREFIX time: <http://www.w3.org/2006/time#> PREFIX ex:   <http://example/> SELECT ?t ?heure WHERE{?h ex:at \"Saint-Etienne\". ?h ex:hasTemp ?t. ?h time:hours ?heure.}";
         String query2 = "PREFIX ex:   <http://example/> PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX sosa: <http://www.w3.org/ns/sosa/> PREFIX time: <http://www.w3.org/2006/time#> SELECT ?t ?v WHERE {?r rdf:type sosa:Result. ?r ex:hasType \"Cel\". ?r sosa:isResultOf ?o. ?o sosa:resultTime ?t. ?r ex:hasValue ?v.}";
         ResultSet res = conn.execReturn(query);
-        conn.execSelectAndPrint(query2);
-        float totalTemp = 0;
+        ResultSet res2 = conn.execReturn(query2);
+        float temp = 0;
         int nbRes = 0;
+        int n = 0;
+        String tempsSensor = "";
+        float tempeSensor = 0;
         while (res.hasNext()) {
             QuerySolution soln = res.nextSolution();
-            totalTemp += Float.parseFloat(soln.get("t").toString().split(" ")[0]);
-            nbRes++;
+            temp = Float.parseFloat(soln.get("t").toString().split(" ")[0]);
+            int heure = Integer.parseInt(soln.get("heure").toString().split(" ")[0]);
+            System.out.println("temp : " + temp + " heure : " + heure);
 
         }
-        System.err.println("Moyenne : " + (totalTemp / nbRes));
+
+        while (res2.hasNext()) {
+
+            QuerySolution soln = res2.nextSolution();
+            tempsSensor = soln.get("t").asLiteral().getString();
+            tempeSensor = Float.parseFloat(soln.get("v").asLiteral().getString());
+            String salle = soln.get("v").asLiteral().getString();
+            if (n < 100) {
+                System.out.println("boucle " + tempsSensor + " " + tempeSensor + " Salle: " + salle);
+                n++;
+            }
+        }
+        System.out.println("fin " + tempsSensor + " " + tempeSensor);
         return "temperature.html";
     }
 }
